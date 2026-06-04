164a5c67c31e80d8b010932413d8c0aa806f2b6d
tofacool1-debug <tofacool1@gmail.com>
2026-06-05 01:26:55 +0700

    Add Arabic verb data structure in data.js

---
+++ data.js
@@ -0,0 +1,36 @@
+const dataBab = {
+  "bab1": {
+    "ا": [
+      {madhi:"أَكَلَ", fa:"أ", ain:"ك", lam:"ل", illat:"salim", arti:"makan"},
+      {madhi:"أَخَذَ", fa:"أ", ain:"خ", lam:"ذ", illat:"mahmuz_fa", arti:"mengambil"},
+      {madhi:"أَمَرَ", fa:"أ", ain:"م", lam:"ر", illat:"mahmuz_fa", arti:"memerintah"},
+      {madhi:"أَبَى", fa:"أ", ain:"ب", lam:"ى", illat:"naqish_ya", arti:"menolak"},
+      {madhi:"أَتَى", fa:"أ", ain:"ت", lam:"ى", illat:"naqish_ya", arti:"datang"},
+      {madhi:"أَدَّى", fa:"أ", ain:"د", lam:"ى", illat:"naqish_ya", arti:"menunaikan"},
+      {madhi:"أَسَأَ", fa:"أ", ain:"س", lam:"أ", illat:"mahmuz_lam", arti:"berbuat jahat"},
+      {madhi:"أَوَى", fa:"أ", ain:"و", lam:"ى", illat:"lafif_maqrun", arti:"berlindung"},
+      {madhi:"أَبَّ", fa:"أ", ain:"ب", lam:"ب", illat:"mudhoaf", arti:"berjalan cepat"},
+      {madhi:"أَمَّ", fa:"أ", ain:"م", lam:"م", illat:"mudhoaf", arti:"mengimami"},
+      {madhi:"أَدُوَ", fa:"أ", ain:"د", lam:"و", illat:"naqish_waw", arti:"sakit"},
+      {madhi:"أَلُوَ", fa:"أ", ain:"ل", lam:"و", illat:"naqish_waw", arti:"berkeluh kesah"},
+      {madhi:"أَشَى", fa:"أ", ain:"ش", lam:"ى", illat:"naqish_ya", arti:"mengadu"}
+    ]
+  };
+  "bab1": {
+    "ب": [
+      {madhi:"بَحَثَ", fa:"ب", ain:"ح", lam:"ث", illat:"salim", arti:"mencari"}, // يَبْحُثُ
+      {madhi:"بَذَلَ", fa:"ب", ain:"ذ", lam:"ل", illat:"salim", arti:"mengorbankan"}, // يَبْذُلُ
+      {madhi:"بَسَطَ", fa:"ب", ain:"س", lam:"ط", illat:"salim", arti:"membentangkan"}, // يَبْسُطُ
+      {madhi:"بَطَنَ", fa:"ب", ain:"ط", lam:"ن", illat:"salim", arti:"menyembunyikan"}, // يَبْطُنُ
+      {madhi:"بَرَأَ", fa:"ب", ain:"ر", lam:"أ", illat:"mahmuz_lam", arti:"sembuh"}, // يَبْرُؤُ
+      {madhi:"بَدَأَ", fa:"ب", ain:"د", lam:"أ", illat:"mahmuz_lam", arti:"memulai"}, // يَبْدَؤُ
+      {madhi:"بَغَى", fa:"ب", ain:"غ", lam:"ى", illat:"naqish_ya", arti:"berbuat dzolim"}, // يَبْغُو
+      {madhi:"بَكَى", fa:"ب", ain:"ك", lam:"ى", illat:"naqish_ya", arti:"menangis"}, // يَبْكُو
+      {madhi:"بَنَى", fa:"ب", ain:"ن", lam:"ى", illat:"naqish_ya", arti:"membangun"}, // يَبْنُو
+      {madhi:"بَدَا", fa:"ب", ain:"د", lam:"ا", illat:"naqish_ya", arti:"tampak"}, // يَبْدُو
+      {madhi:"بَرَّ", fa:"ب", ain:"ر", lam:"ر", illat:"mudhoaf", arti:"berbakti"}, // يَبُرُّ
+      {madhi:"بَطَّ", fa:"ب", ain:"ط", lam:"ط", illat:"mudhoaf", arti:"memotong perut"}, // يَبُطُّ
+      {madhi:"بَقَّ", fa:"ب", ain:"ق", lam:"ق", illat:"mudhoaf", arti:"mengeluarkan suara"} // يَبُقُّ
+    ]
+  }
+};
