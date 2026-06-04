d388de6ef9768ffe09e7e2b874dbfe003482e6df
tofacool1-debug <tofacool1@gmail.com>
2026-06-05 01:20:56 +0700

    Add new Arabic words and meanings to data.js

--- tasrifpro/data.js
+++ tasrifpro/data.js
@@ -15,5 +15,22 @@ const dataBab = {
       {madhi:"أَلُوَ", fa:"أ", ain:"ل", lam:"و", illat:"naqish_waw", arti:"berkeluh kesah"},
       {madhi:"أَشَى", fa:"أ", ain:"ش", lam:"ى", illat:"naqish_ya", arti:"mengadu"}
     ]
+  };
+  "bab1": {
+    "ب": [
+      {madhi:"بَحَثَ", fa:"ب", ain:"ح", lam:"ث", illat:"salim", arti:"mencari"}, // يَبْحُثُ
+      {madhi:"بَذَلَ", fa:"ب", ain:"ذ", lam:"ل", illat:"salim", arti:"mengorbankan"}, // يَبْذُلُ
+      {madhi:"بَسَطَ", fa:"ب", ain:"س", lam:"ط", illat:"salim", arti:"membentangkan"}, // يَبْسُطُ
+      {madhi:"بَطَنَ", fa:"ب", ain:"ط", lam:"ن", illat:"salim", arti:"menyembunyikan"}, // يَبْطُنُ
+      {madhi:"بَرَأَ", fa:"ب", ain:"ر", lam:"أ", illat:"mahmuz_lam", arti:"sembuh"}, // يَبْرُؤُ
+      {madhi:"بَدَأَ", fa:"ب", ain:"د", lam:"أ", illat:"mahmuz_lam", arti:"memulai"}, // يَبْدَؤُ
+      {madhi:"بَغَى", fa:"ب", ain:"غ", lam:"ى", illat:"naqish_ya", arti:"berbuat dzolim"}, // يَبْغُو
+      {madhi:"بَكَى", fa:"ب", ain:"ك", lam:"ى", illat:"naqish_ya", arti:"menangis"}, // يَبْكُو
+      {madhi:"بَنَى", fa:"ب", ain:"ن", lam:"ى", illat:"naqish_ya", arti:"membangun"}, // يَبْنُو
+      {madhi:"بَدَا", fa:"ب", ain:"د", lam:"ا", illat:"naqish_ya", arti:"tampak"}, // يَبْدُو
+      {madhi:"بَرَّ", fa:"ب", ain:"ر", lam:"ر", illat:"mudhoaf", arti:"berbakti"}, // يَبُرُّ
+      {madhi:"بَطَّ", fa:"ب", ain:"ط", lam:"ط", illat:"mudhoaf", arti:"memotong perut"}, // يَبُطُّ
+      {madhi:"بَقَّ", fa:"ب", ain:"ق", lam:"ق", illat:"mudhoaf", arti:"mengeluarkan suara"} // يَبُقُّ
+    ]
   }
 };
