8eec64d12775d3544e4b79b9b8e2f8896ab1350d
tofacool1-debug <tofacool1@gmail.com>
2026-06-05 01:16:07 +0700

    Add dataBab object with Arabic verb data

---
+++ tasrifpro/data.js
@@ -0,0 +1,19 @@
+const dataBab = {
+  "bab1": {
+    "ا": [
+      {madhi:"أَكَلَ", fa:"أ", ain:"ك", lam:"ل", illat:"salim", arti:"makan"},
+      {madhi:"أَخَذَ", fa:"أ", ain:"خ", lam:"ذ", illat:"mahmuz_fa", arti:"mengambil"},
+      {madhi:"أَمَرَ", fa:"أ", ain:"م", lam:"ر", illat:"mahmuz_fa", arti:"memerintah"},
+      {madhi:"أَبَى", fa:"أ", ain:"ب", lam:"ى", illat:"naqish_ya", arti:"menolak"},
+      {madhi:"أَتَى", fa:"أ", ain:"ت", lam:"ى", illat:"naqish_ya", arti:"datang"},
+      {madhi:"أَدَّى", fa:"أ", ain:"د", lam:"ى", illat:"naqish_ya", arti:"menunaikan"},
+      {madhi:"أَسَأَ", fa:"أ", ain:"س", lam:"أ", illat:"mahmuz_lam", arti:"berbuat jahat"},
+      {madhi:"أَوَى", fa:"أ", ain:"و", lam:"ى", illat:"lafif_maqrun", arti:"berlindung"},
+      {madhi:"أَبَّ", fa:"أ", ain:"ب", lam:"ب", illat:"mudhoaf", arti:"berjalan cepat"},
+      {madhi:"أَمَّ", fa:"أ", ain:"م", lam:"م", illat:"mudhoaf", arti:"mengimami"},
+      {madhi:"أَدُوَ", fa:"أ", ain:"د", lam:"و", illat:"naqish_waw", arti:"sakit"},
+      {madhi:"أَلُوَ", fa:"أ", ain:"ل", lam:"و", illat:"naqish_waw", arti:"berkeluh kesah"},
+      {madhi:"أَشَى", fa:"أ", ain:"ش", lam:"ى", illat:"naqish_ya", arti:"mengadu"}
+    ]
+  }
+};
